c557102b7bf61894de26c4a65847e37d22d3cf6b
Matthew Lyons <mattewjosephlyons@gmail.com>
2023-05-15 15:53:28 -0500

    Create RootLayout

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from 'react';
+import '@styles/globals.css';
+
+export const metadata = {
+  title: 'Promptopia',
+  description: 'Discover & Share AI Prompts',
+};
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
