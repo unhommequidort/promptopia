27628e7efc3e0b267df5d5249758da98ab9e88e4
Matthew Lyons <mattewjosephlyons@gmail.com>
2023-05-16 10:57:54 -0500

    Don't make username unique

--- models/user.ts
+++ models/user.ts
@@ -15,7 +15,6 @@ const userSchema = new Schema<IUser>({
   username: {
     type: String,
     required: [true, 'Username is required!'],
-    unique: true,
     match: [
       /^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
       'Username invalid, it should contain 8-20 alphanumeric letters and be unique!',
