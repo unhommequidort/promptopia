17fd0e6dab9e58e9daf17f39191ad9a8a9bd5722
Matthew Lyons <mattewjosephlyons@gmail.com>
2023-05-17 14:17:04 -0500

    Add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from 'react';
 import '@styles/globals.css';
 import Nav from '@components/Nav';
 import Provider from '@components/Provider';
+import Head from 'next/head';
 
 export const metadata = {
   title: 'Promptopia',
@@ -11,6 +12,29 @@ export const metadata = {
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <html lang="en">
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <body>
         <Provider>
           <div className="main">
